6ca1631210477795a0386694236393a0a4810d1d
Ate Mochel <>
2021-04-02 00:12:27 +0000

    Begin ghost 2

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ function draw() {
     levelnumber = 1;
    
     ghostMove();
+    ghost2Move();
 
 	  }
 
@@ -93,6 +94,8 @@ function draw() {
     levelnumber = 2;
     
     ghostMove();
+    ghost2Move();
+
 
     
 	  }
@@ -107,6 +110,7 @@ function draw() {
     levelnumber = 3;
    
     ghostMove();
+    ghost2Move();
 
   }
 
@@ -512,6 +516,7 @@ function ghostMove() {
         level[ghost.y][ghost.x] = 3;
         }
         else if (level[ghost.y-1][ghost.x] === 0) { 
+        level[ghost.y][ghost.x] = 0;
         ghost.y = ghost.y - 1;
         level[ghost.y][ghost.x] = 3;
         }
@@ -526,8 +531,72 @@ function ghostMove() {
         level[ghost.y][ghost.x] = 3;
         }
         else if (level[ghost.y+1][ghost.x] === 0) { 
+        level[ghost.y][ghost.x] = 0;
         ghost.y = ghost.y + 1;
-       level[ghost.y][ghost.x] = 3;
+        level[ghost.y][ghost.x] = 3;
+        }
+        drawWorld();
+      }
+      break;
+      }
+  }
+
+function ghost2Move() {
+  switch (getRndInteger2()) {
+    case 1:
+      if (level[ghost.y][ghost.x-1] !== 1){
+        if (level[ghost.y][ghost.x-1] === 2) {
+         level[ghost.y][ghost.x] = 2;
+         ghost.x = ghost.x - 1;
+         level[ghost.y][ghost.x] = 4;
+        }
+        else if (level[ghost.y][ghost.x-1] === 0) { 
+         level[ghost.y][ghost.x] = 0;
+         ghost.x = ghost.x - 1;
+         level[ghost.y][ghost.x] = 4;
+        }
+       drawWorld();
+      }
+      break;
+    case 2: 
+    if (level[ghost.y][ghost.x+1] !== 1){
+      if (level[ghost.y][ghost.x+1] === 2) {         
+         level[ghost.y][ghost.x] = 2;
+         ghost.x = ghost.x + 1;
+         level[ghost.y][ghost.x] = 4;
+        }
+        else if (level[ghost.y][ghost.x+1] === 0) { 
+         level[ghost.y][ghost.x] = 0;
+         ghost.x = ghost.x + 1;
+         level[ghost.y][ghost.x] = 4;
+        }   
+       drawWorld();
+      }
+      break;
+    case 3:     
+    if (level[ghost.y-1][ghost.x] !== 1){
+      if (level[ghost.y-1][ghost.x] === 2) {   
+        level[ghost.y][ghost.x] = 2;    
+        ghost.y = ghost.y - 1;
+        level[ghost.y][ghost.x] = 4;
+        }
+        else if (level[ghost.y-1][ghost.x] === 0) { 
+        ghost.y = ghost.y - 1;
+        level[ghost.y][ghost.x] = 4;
+        }
+       drawWorld();
+      } 
+      break;
+    case 4:
+    if (level[ghost.y+1][ghost.x] !== 1){
+      if (level[ghost.y+1][ghost.x] === 2) { 
+        level[ghost.y][ghost.x] = 2;      
+        ghost.y = ghost.y + 1;
+        level[ghost.y][ghost.x] = 4;
+        }
+        else if (level[ghost.y+1][ghost.x] === 0) { 
+        ghost.y = ghost.y + 1;
+       level[ghost.y][ghost.x] = 4;
         }
         drawWorld();
       }
@@ -537,6 +606,10 @@ function ghostMove() {
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (5) ) + 1;
+}
+
+function getRndInteger2(min, max) {
+  return Math.floor(Math.random() * (1) ) + 1;
 }
 
 
